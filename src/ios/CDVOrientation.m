47a72d867557bd062323fcf22590b9ffe7ea1f19
Michael Rahn <michael.rahn@digistore24.team>
2021-02-15 18:12:20 +0100

    fix: do not automatically change orientation to lastOrientation.

--- src/ios/CDVOrientation.m
+++ src/ios/CDVOrientation.m
@@ -48,7 +48,7 @@
         }
     } else {
         if (_lastOrientation != UIInterfaceOrientationUnknown) {
-            [[UIDevice currentDevice] setValue:[NSNumber numberWithInt:_lastOrientation] forKey:@"orientation"];
+            // [[UIDevice currentDevice] setValue:[NSNumber numberWithInt:_lastOrientation] forKey:@"orientation"];
             ((void (*)(CDVViewController*, SEL, NSMutableArray*))objc_msgSend)(vc,selector,result);
             [UINavigationController attemptRotationToDeviceOrientation];
         }
